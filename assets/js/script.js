3374edc45424811848647834dec1a50536283693
kingkite <kitewebdevelopers@gmail.com>
2023-09-22 19:52:48 +0100

    Adding the refund page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// ********* Handle Navigation Start
 const menuContainer = document.querySelector('#menu-container');
 const openMenuBtn = document.querySelector('#open-menu-button');
 const closeMenuBtn = document.querySelector('#close-menu-button');
@@ -8,7 +9,7 @@ function closeMenu() {
 	// close the menu
 	// Add the scroll back in for mobile and medium screens
 	const body = document.querySelector('body');
-	body.classList.remove('overflow-y-hidden', 'lg:overflow-y-auto');
+	body.classList.remove('hide-scrollbar');
 }
 
 // Open the menu container
@@ -17,7 +18,7 @@ openMenuBtn.addEventListener('click', function () {
 
 	// Remove the scroll from the body element for mobile and medium screens
 	const body = document.querySelector('body');
-	body.classList.add('overflow-y-hidden', 'lg:overflow-y-auto');
+	body.classList.add('hide-scrollbar');
 });
 
 // Close the menu container
@@ -40,3 +41,51 @@ menuItems.forEach((item) => {
 		closeMenu();
 	});
 });
+
+// ************ Handle Navigation Stop
+
+
+// *********** Handle Frequently Asked Questions Start
+
+const faqs = document.querySelectorAll('.faq');
+
+// Add an onclick event listener that gets the data-id
+// attribute from an element
+faqs.forEach((faq) => {
+	faq.addEventListener('click', function () {
+		const id = +this.getAttribute('data-id');
+		if (isNaN(id)) return;
+
+		// Loop through the questions and find the question that
+		// owns the id
+
+		let question = null
+		faqs.forEach(function(item) {
+			const questionId = +item.getAttribute('data-id');
+
+			// If a question has already been found, return;
+			if (question) return;
+			if (isNaN(questionId)) return;
+			if (questionId !== id) return;
+
+			question = item
+		})
+
+		if (!question) return;
+
+		question.classList.toggle('show');
+		question = null;
+
+		// Close any other question that might be opened
+		faqs.forEach(faq => {
+			const questionId = +faq.getAttribute('data-id');
+			if (isNaN(questionId)) return;
+
+			if (questionId !== id && faq.classList.contains('show')) {
+				faq.classList.remove('show')
+			}
+		})
+	})
+})
+
+// *********** Handle Frequently Asked Questions Stop
